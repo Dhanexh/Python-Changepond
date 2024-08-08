4ef3f796010ee8502eb2a25528f4e2873cca0df5
Dhanesh A <dhanesh.a@ctlep.local>
2024-08-08 12:28:19 +0530

    dfghf

--- Oops/task_8.py
+++ Oops/task_8.py
@@ -2,43 +2,49 @@
 # Which include methods for adding items , removing items , 
 # total items and total price
 
-class ShoppingCart:
-    def __init__(self):
-        self.items = {}
+class Shopping_card:
+    items = {
+        "apple":100,
+        "orange":300,
+        "grapes":130,
+        "cherry":130,
+        "banana":140,
+        "guava":105,
+        "watermelon":150,   
+             }
+    
+    user_items = {}
 
-    def add_item(self, item_name, price, quantity=1):
-        if item_name in self.items:
-            self.items[item_name]['quantity'] += quantity
-        else:
-            self.items[item_name] = {'price': price, 'quantity': quantity}
+    def display(self):
+        print(self.items)
 
-    def remove_item(self, item_name, quantity=1):
-        if item_name in self.items:
-            if self.items[item_name]['quantity'] <= quantity:
-                del self.items[item_name]
-            else:
-                self.items[item_name]['quantity'] -= quantity
+    def user_display(self):
+        print(self.user_items)
+
+    def adding_items(self,items):
+        self.user_items[items] = self.items[items]
+
+    def remove_items(self,items):
+        del self.user_items[items]
 
     def total_items(self):
-        return sum(details['quantity'] for details in self.items.values())
+        print(f"Total Items { len(self.user_items.values())}")
 
     def total_price(self):
-        return sum(details['price'] * details['quantity'] for details in self.items.values())
+        finals = sum(self.user_items.values())
+        print("Total price",finals)
 
-
-
-if __name__ == "__main__":
-    cart = ShoppingCart()
-    cart.add_item('Apple', 20, 3)
-    cart.add_item('Banana', 10, 2)
-    print(cart.items)
-    print(f"Total items: {cart.total_items()}")
-    print(f"Total price: ${cart.total_price()}")
-
-    cart.remove_item('Apple', 1)
-    print("\nAfter removing 1 Apple:")
-    print(cart.items)
-    print(f"Total items: {cart.total_items()}")
-    print(f"Total price: ${cart.total_price()}")
-
+Obj1 =Shopping_card()
+Obj1.display()
+Obj1.adding_items("apple")
+Obj1.user_display()
+Obj1.adding_items("orange")
+Obj1.user_display()
+Obj1.adding_items("banana")
+Obj1.user_display()
+Obj1.total_items()
+Obj1.total_price()
+Obj1.remove_items("apple")
+Obj1.total_items()
+Obj1.total_price()
 
